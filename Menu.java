3f6bb432ef7aa3fa53e13d20ebef9f71ed29de7b
Dennis Garro <sistemasitm6099@gmail.com>
2025-02-17 07:06:39 -0500

    creacion del menu

---
+++ Menu.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Menu {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int opt = 0, d = 0;
+        System.out.println("Ingrese el punto que desea validar");
+        opt = sc.nextInt();
+        System.out.println("ingrese la dimension de la matriz");
+        d = sc.nextInt();
+        switch (opt) {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+            case 7:
+                break;
+            case 8:
+                break;
+            case 9:
+                break;
+            case 10:
+                break;
+
+            default:
+                break;
+        }
+    }
+}
